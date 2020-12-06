ac354a5538a5d8cd1e052ef552bbe51c1ccc3664
ilkerispir <ilker.ispir@alo-tech.com>
2020-12-06 14:33:25 +0300

    feat: crated server.js & Dockerfile

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "hello world!"
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
