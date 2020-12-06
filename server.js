0f10cc33cc8ec5d6c240155ef862fa1402cb3137
ilkerispir <ilker.ispir@alo-tech.com>
2020-12-07 00:06:34 +0300

    fix: modified server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "hello world!",
-    });
+app.get('/', async (req, res) => {
+  var config = {
+    method: 'GET',
+    url: 'https://webhook.site/1b77942e-74a1-47af-b13e-a60dd4f4d3f2',
+  };  
+  var reponse = await axios(config);
+
+  res.status(200).json({
+    success: true
+  });
 });
 
 app.listen(port, () => {
